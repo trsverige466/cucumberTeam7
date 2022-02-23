a56604f7f4355ac3fac4b9422a634afa9c89bee7
Abdulbakı Gumus <agummus038@gmail.com>
2022-02-23 13:01:37 +0300

    yusuf abı affet

--- src/test/java/pages/HMCpage.java
+++ src/test/java/pages/HMCpage.java
@@ -3,11 +3,15 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
 
+import java.util.List;
+import java.util.Random;
+
 public class HMCpage {
-    public HMCpage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public HMCpage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "(//a[@class='nav-link'])[7]")
@@ -21,4 +25,21 @@ public class HMCpage {
 
     @FindBy(xpath = "//input[@id='Password']")
     public WebElement registrationPassword;
+
+    @FindBy(xpath = "(//select[@class='form-control input-lg required'])[1]")
+    public WebElement countryBoxElement;
+
+
+    public void dropDownRandom(WebElement dropDown) {
+        Select select = new Select(dropDown);
+        List<WebElement> dropDownList = select.getOptions();
+        Random random = new Random();
+        int country = random.nextInt(1, dropDownList.size() - 1);
+        select.selectByIndex(country);
+
+
+    }
+
+
+
 }
--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features="src/test/resources/features",
         glue = "stepdefinitions",
-        tags="@Dinamik",
+        tags="@t1",
         dryRun = false
 )
 
